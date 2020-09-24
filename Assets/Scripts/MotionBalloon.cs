5d8cccf3015b111f70055924549ce85c1731c58f
Саша Филипенко <kingsans12345@gmail.com>
2020-09-24 11:51:38 +0300

    first commit and fly balloons

---
+++ Assets/Scripts/MotionBalloon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Security.Cryptography;
+using UnityEngine;
+using System; 
+
+public class MotionBalloon : MonoBehaviour
+{
+    int speed;
+    double variableForSine = Math.PI;
+    public float speedForSine, speedInY;
+    const int turnSpeed = -1;
+    const float balloonLife = 5f; 
+
+    void Start()
+    {
+        speed = UnityEngine.Random.Range(2, 5);
+        if (transform.position.x > 0) speed *= turnSpeed; 
+        
+    }
+
+    void Update()
+    {
+        variableForSine += speedForSine; 
+        Destroy(gameObject, balloonLife);
+        transform.Translate(Time.deltaTime * speed, speedInY * (float)Math.Sin(variableForSine), 0);
+
+    }
+}
---
+++ Assets/Scripts/SpawnBalloon.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SpawnBalloon : MonoBehaviour
+{
+    public GameObject balloon;
+    const float spawnPointX = 6f, spawnPointY = 4f;
+    const int choicePointSpawn = 1; 
+
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    /// <summary>
+    /// Function for spawn balloon 
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator Spawn()
+    {
+        while (true)
+        {
+            float posistionX;
+            int directionBallooon = Random.Range(0, 2);
+            if (directionBallooon >= choicePointSpawn)
+                posistionX = -spawnPointX;
+            else
+                posistionX = spawnPointX;
+            Vector3 positionSpawn = new Vector3(posistionX, Random.Range(-spawnPointY, spawnPointY), 1);
+            Instantiate(balloon, positionSpawn, Quaternion.identity);
+            yield return new WaitForSeconds(Random.Range(2, 4));
+        }
+    }
+}
